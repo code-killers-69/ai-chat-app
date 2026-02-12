40da1ff01a9eb027c96070983cbcd1551c916c4f
dolmocheng <dolmocheng@tencent.com>
2026-02-12 14:55:02 +0800

    完善文档

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: urls.local,
+        target: urls.prod2,
         changeOrigin: true,
       },
     },
