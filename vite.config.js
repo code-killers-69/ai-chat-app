bbe79f48f9d94ae1d5021c781238442808c0a868
dealBreaker2003 <shuchengjin777@gmail.com>
2026-03-21 14:16:56 +0800

    feat: 对包体积进行gzip压缩，手动分包，逻辑和依赖分离

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,44 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240, // 只有超过 10k 的文件才压缩
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    // 生产环境移除 console 和 debugger
+    esbuild: {
+      drop: ['console', 'debugger'],
+    },
+    rollupOptions: {
+      output: {
+        // 静态资源分类打包
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        // 手动拆包：将 node_modules 里的依赖单独打包成 vendor chunk
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
